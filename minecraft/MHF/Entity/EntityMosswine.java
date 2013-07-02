24dda2985e5a25f601169564afbc13a0733721b9
LordOfEpicness <Dannyflame98@gmail.com>
2013-07-02 11:46:24 -0400

    Update EntityMosswine.java
    Just put in some code to allow it to eat mushrooms now.

--- minecraft/MHF/Entity/EntityMosswine.java
+++ minecraft/MHF/Entity/EntityMosswine.java
@@ -1,8 +1,11 @@
+
 package MHF.Entity;
 
+import MHF.Entity.AI.EntityAIEatMushrooms;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIEatGrass;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
@@ -13,6 +16,10 @@ public class EntityMosswine extends EntityCreature{
 	/** Above zero if this Mosswine is Angry. */
 	 private int angerLevel = 0;
 	 private int attackTimer;
+	 
+	    private int mosswineTimer;
+
+	    private EntityAIEatMushrooms aiEatMushrooms = new EntityAIEatMushrooms();
 
 	public EntityMosswine(World par1World) {
 		super(par1World);
@@ -23,9 +30,43 @@ public class EntityMosswine extends EntityCreature{
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(1, new EntityAIWander(this, this.moveSpeed));
 		this.tasks.addTask(3, new EntityAIAttackOnCollide(this, moveSpeed, true));
-		this.targetTasks.addTask(4, new EntityAIHurtByTarget(this, true));
+		this.tasks.addTask(4, new EntityAIEatMushrooms());
+		this.targetTasks.addTask(5, new EntityAIHurtByTarget(this, true));
 		
 	}
+	
+	 protected void updateAITasks()
+	    {
+	        this.mosswineTimer = this.aiEatMushrooms.getEatGrassTick();
+	        super.updateAITasks();
+	    }
+
+	    public void onLivingUpdate()
+	    {
+	        if (this.worldObj.isRemote)
+	        {
+	            this.mosswineTimer = Math.max(0, this.mosswineTimer - 1);
+	        }
+
+	        super.onLivingUpdate();
+	        
+	        if (this.attackTimer > 0)
+	        {
+	            --this.attackTimer;
+	        }
+	        if (this.motionX * this.motionX + this.motionZ * this.motionZ > 2.500000277905201E-7D && this.rand.nextInt(5) == 0)
+	        {
+	            int var1 = MathHelper.floor_double(this.posX);
+	            int var2 = MathHelper.floor_double(this.posY - 0.20000000298023224D - (double)this.yOffset);
+	            int var3 = MathHelper.floor_double(this.posZ);
+	            int var4 = this.worldObj.getBlockId(var1, var2, var3);
+
+	            if (var4 > 0)
+	            {
+	                this.worldObj.spawnParticle("tilecrack_" + var4 + "_" + this.worldObj.getBlockMetadata(var1, var2, var3), this.posX + ((double)this.rand.nextFloat() - 0.5D) * (double)this.width, this.boundingBox.minY + 0.1D, this.posZ + ((double)this.rand.nextFloat() - 0.5D) * (double)this.width, 4.0D * ((double)this.rand.nextFloat() - 0.5D), 0.5D, ((double)this.rand.nextFloat() - 0.5D) * 4.0D);
+	            }
+	        }
+	    }
 	
 	public boolean isAIEnabled()
     {
@@ -39,31 +80,7 @@ public class EntityMosswine extends EntityCreature{
 	
 	public int getMaxHealth() {
 		return 10;
-	}
-	
-	public void onLivingUpdate()
-    {
-        super.onLivingUpdate();
-
-        if (this.attackTimer > 0)
-        {
-            --this.attackTimer;
-        }
-        if (this.motionX * this.motionX + this.motionZ * this.motionZ > 2.500000277905201E-7D && this.rand.nextInt(5) == 0)
-        {
-            int var1 = MathHelper.floor_double(this.posX);
-            int var2 = MathHelper.floor_double(this.posY - 0.20000000298023224D - (double)this.yOffset);
-            int var3 = MathHelper.floor_double(this.posZ);
-            int var4 = this.worldObj.getBlockId(var1, var2, var3);
-
-            if (var4 > 0)
-            {
-                this.worldObj.spawnParticle("tilecrack_" + var4 + "_" + this.worldObj.getBlockMetadata(var1, var2, var3), this.posX + ((double)this.rand.nextFloat() - 0.5D) * (double)this.width, this.boundingBox.minY + 0.1D, this.posZ + ((double)this.rand.nextFloat() - 0.5D) * (double)this.width, 4.0D * ((double)this.rand.nextFloat() - 0.5D), 0.5D, ((double)this.rand.nextFloat() - 0.5D) * 4.0D);
-            }
-        }
-        
-    }
-        
+	}   
 	
 	public int getTotalArmorValue()
     {
